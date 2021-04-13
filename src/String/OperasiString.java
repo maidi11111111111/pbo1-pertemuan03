6dfee93cb998e66553868b2868704cd7a91922b6
maidi11111111111 <maidi11.mm@gmail.com>
2021-04-13 12:32:35 +0800

    Membuat String dari array Char

--- src/String/OperasiString.java
+++ src/String/OperasiString.java
@@ -6,5 +6,9 @@ public class OperasiString {
 
         System.out.println(kota);
 
+        char[] uniskaChar = { 'U', 'N', 'I', 'S', 'K', 'A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+
     }
 }
