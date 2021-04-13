4df81be44c538391dc6bbeb178a108918bcb3c78
maidi11111111111 <maidi11.mm@gmail.com>
2021-04-13 12:51:01 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = " Maidi";
+    int jumlahSks = 24;
+    double indeksPrestasiJumulatif = 3.89;
+}
