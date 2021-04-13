38930f2a7b35f75840d0ef12ff51ef143d5306d6
maidi11111111111 <maidi11.mm@gmail.com>
2021-04-13 12:54:15 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,13 @@ public class OperatorBinary {
     String nama = " Maidi";
     int jumlahSks = 24;
     double indeksPrestasiJumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
